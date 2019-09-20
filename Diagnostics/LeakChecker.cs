6ab156c04f09539a86e4e5d93001e8b7c2ce4be5
alice_alias <alice-alias@arith.jp>
2019-09-20 16:34:56 +0900

    うごくものとする

---
+++ Diagnostics/LeakChecker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace NightCore.Diagnostics
+{
+    public static class LeakChecker
+    {
+        public static bool DebugWrite { get; set; }
+
+        static readonly Dictionary<IntPtr, string> names = new Dictionary<IntPtr, string>();
+
+        public static void Construct(GCHandle handle, string name)
+            => Construct(GCHandle.ToIntPtr(handle), name);
+
+        public static void Construct(IntPtr handle, string name)
+        {
+            lock (names)
+            {
+                names[handle] = name;
+
+                if (DebugWrite)
+                    Debug.WriteLine($"Constructed #{handle.ToString("x08")}: {name}");
+            }
+        }
+
+        public static void Destruct(GCHandle handle)
+            => Destruct(GCHandle.ToIntPtr(handle));
+
+        public static void Destruct(IntPtr handle)
+        {
+            lock (names)
+            {
+                if (names.TryGetValue(handle, out var name) && DebugWrite)
+                    Debug.WriteLine($"Destructed #{handle.ToString("x08")}: {name}");
+
+                names.Remove(handle);
+            }
+        }
+        public static void NotifyLeak(GCHandle handle)
+            => NotifyLeak(GCHandle.ToIntPtr(handle));
+
+        public static void NotifyLeak(IntPtr handle)
+        {
+            lock (names)
+            {
+                if (names.TryGetValue(handle, out var name) && DebugWrite)
+                    Debug.WriteLine($"Automatically destructed #{handle.ToString("x08")}: {name}");
+            }
+        }
+
+        public static void PrintAliveObjects() => PrintAliveObjects(x => Debug.WriteLine(x));
+
+        public static void PrintAliveObjects(Action<string> writeLine)
+        {
+            lock (names)
+            {
+                if (names.Count == 0)
+                {
+                    writeLine("No leaked object.");
+                }
+                else
+                {
+                    writeLine("Leaked objects:");
+                    foreach (var d in names.ToArray())
+                        writeLine($"  #{d.Key.ToString("x08")}: {d.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ DisposableObject.cs
@@ -0,0 +1,43 @@
+﻿using NightCore.Diagnostics;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace NightCore
+{
+    public class DisposableObject : IDisposable
+    {
+        int disposed;
+        public bool IsDisposed => disposed != 0;
+        readonly GCHandle handle;
+
+        public DisposableObject()
+        {
+            handle = GCHandle.Alloc(this, GCHandleType.Weak);
+            LeakChecker.Construct(handle, GetType().FullName);
+        }
+
+        protected virtual void Dispose(bool disposing) { }
+
+        public event EventHandler Disposed;
+
+        protected virtual void OnDisposed() => Disposed?.Invoke(this, EventArgs.Empty);
+
+        ~DisposableObject()
+        {
+            Dispose(false);
+            LeakChecker.NotifyLeak(handle);
+        }
+
+        public void Dispose()
+        {
+            if (Interlocked.Exchange(ref disposed, 1) == 0)
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+                OnDisposed();
+                LeakChecker.Destruct(handle);
+            }
+        }
+    }
+}
